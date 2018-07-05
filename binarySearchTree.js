dfd9d539ee0f3939eaccb3b32ab322f1825b89ab
Sarthak <sarthakn18@gmail.com>
2018-07-05 18:40:58 +0530

    Depth first traversal and logic to find max and min node in the tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -44,10 +44,41 @@ BST.prototype.depthFirstTraversal = function(iteratorFunc, order){
     if (order === 'post-order') iteratorFunc(this.value, order);
 }
 
+BST.prototype.breadthFirstTraversal = function(iteratorFunc){
+    var queue = [this];
+    while (queue.length){
+        var treeNode = queue.shift();
+        iteratorFunc(treeNode);
+        if (treeNode.left) queue.push(treeNode.left);
+        if (treeNode.right) queue.push(treeNode.right);
+    }
+}
+
+BST.prototype.getMinVal = function(){
+    this.breadthFirstTraversal(findMin);
+};
+
+BST.prototype.getMaxVal = function(){
+    this.breadthFirstTraversal(findMax);
+};
+
+BST.prototype.minVal = null;
+BST.prototype.maxVal = null;
+
+function findMin(node){
+    if (BST.prototype.minVal == null) BST.prototype.minVal = node.value;
+    if (BST.prototype.minVal > node.value) BST.prototype.minVal = node.value;
+}
+
+function findMax(node){
+    if (BST.prototype.maxVal == null) BST.prototype.maxVal = node.value;
+    if (BST.prototype.maxVal < node.value) BST.prototype.maxVal = node.value;
+}
+
 var bst = new BST(50);
 
-bst.insert(70);
-bst.insert(40);
+bst.insert(0);
+bst.insert(10);
 bst.insert(45);
 bst.insert(60);
 bst.insert(90);
@@ -55,6 +86,11 @@ bst.insert(85);
 console.log(bst.contains(90));
 
 function log(value){
-    console.log(value);
+    console.log(value.value);
 }
-bst.depthFirstTraversal(log, 'post-order');
+// bst.depthFirstTraversal(log, 'post-order');
+
+bst.breadthFirstTraversal(log);
+console.log(bst.maxVal);
+bst.getMaxVal();
+console.log(bst.maxVal);
